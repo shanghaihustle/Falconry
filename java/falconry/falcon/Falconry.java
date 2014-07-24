f87786ecced4d2d1826e5e3077e696dbd40b792b
Clarky158 <benclark.btc@gmail.com>
2014-07-24 16:39:21 +0100

    fixed typos

--- java/falconry/falcon/Falconry.java
+++ java/falconry/falcon/Falconry.java
@@ -14,7 +14,7 @@ import net.minecraft.item.Item;
 /**
  * Created by Clarky158 on 24/07/2014.
  * <p/>
- * Electrolysm is an open source Minecraft mod
+ * Falconry is an open source Minecraft mod
  * released under version 3 of the GNU Lesser
  * General Public License. This means that
  * the source of this mod is publicly available
--- java/falconry/falcon/proxy/ClientProxy.java
+++ java/falconry/falcon/proxy/ClientProxy.java
@@ -10,7 +10,7 @@ import falconry.falcon.entity.EntityFalcon;
 /**
  * Created by Clarky158 on 24/07/2014.
  * <p/>
- * Electrolysm is an open source Minecraft mod
+ * Falconry is an open source Minecraft mod
  * released under version 3 of the GNU Lesser
  * General Public License. This means that
  * the source of this mod is publicly available
